a6bbc9e222904752844a303efce89509858cd61b
PranjeetGoswami <prgoswami2020@gmail.com>
2025-02-01 01:45:37 +0530

    created navbar and added styles and assets for project

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import About from "../components/About";
+import Contact from "../components/Contact";
+import Experience from "../components/Experience";
+import Feedbacks from "../components/Feedbacks";
+import Hero from "../components/Hero";
+import Loader from "../components/Loader";
+import Navbar from "../components/Navbar";
+import Tech from "../components/Tech";
+import Works from "../components/Works";
+
+// Canvas
+import Ball from "../components/Ball";
+import Computer from "../components/Computers";
+import Earth from "../components/Earth";
+import Stars from "../components/Stars";
+
+export {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Loader,
+  Navbar,
+  Tech,
+  Works,
+
+// Canvas
+  Ball,
+  Computer,
+  Earth,
+  Stars
+};
