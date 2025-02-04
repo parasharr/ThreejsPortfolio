771fda8d717a0d55fbd6626897e594f1cf422780
PranjeetGoswami <prgoswami2020@gmail.com>
2025-02-04 12:21:13 +0530

    added Ball.jsx and Experience.jsx

--- constants/index.js
+++ constants/index.js
@@ -46,15 +46,15 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "React Developer",
       icon: mobile,
     },
     {
-      title: "Backend Developer",
+      title: "UI/UX Developer",
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Front-end Developer",
       icon: creator,
     },
   ];
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Ball from './Ball.jsx'
+import {SectionWrapper } from '../hoc'
+import {technologies} from '../../constants'
+
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology)=> (
+        <div className='w-28 h-28 ' key={technology.name}>
+          <Ball icon={technology.icon}/>
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper (Tech, "")
